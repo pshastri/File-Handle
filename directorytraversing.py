96535798ac5cbb7b22a4bb9c9977d2b9f0dd332e
pshastri <Praveen.Shastri2@microfocus.com>
2017-02-22 11:47:43 +0530

    Commit the change

--- directorytraversing.py
+++ directorytraversing.py
@@ -7,4 +7,5 @@ for root,dirs,files in os.walk("C:\\Users\\pshastri\\Documents\\GitHub\\Datastru
 		print "This is a file: ", myFile
 	for mydir in dirs:
 		print "This is a Directory", mydir
-	
+	# print dirs
+	# print files
